a75b4117a93aa063bc05f54c387b693b9dc5ab02
Tao Wen <1325772089@qq.com>
2020-05-12 00:48:55 +0800

    update

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-// const MiniCssExtract = require('mini-css-extract-plugin') // 引入插件
 
 const path = require("path");
 const {getEntries, getName, getPageConfigs} = require("./utils")
@@ -10,6 +9,7 @@ const {getEntries, getName, getPageConfigs} = require("./utils")
 const entrieConfigs = getEntries("src/*/config.json");
 const { pagesConfig, entries } = getPageConfigs(entrieConfigs)
 console.log(pagesConfig)
+const themeEntries = { index: path.resolve(__dirname, '../template/main.js') }
 
 
 const mode = process.env.NODE_ENV || "development";
@@ -17,7 +17,7 @@ const mode = process.env.NODE_ENV || "development";
 module.exports = {
   mode: mode,
   devtool: "cheap-module-eval-source-map", // 开放环境用，可以看到报错信息准确的排数
-  entry: entries,
+  entry: Object.assign({}, entries, themeEntries),
   output: {
     filename: "[name]/[name].js",
     path: path.join(__dirname, '../dist'),
@@ -115,12 +115,12 @@ module.exports = {
       },
       chunks: ["index"],
     }),
-    new CopyWebpackPlugin([
-      {
-        from: "template/static",
-        to: path.resolve(__dirname, "../dist", "static"),
-        flatten: false,
-      },
-    ]),
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: "template/static",
+    //     to: path.resolve(__dirname, "../dist", "static"),
+    //     flatten: false,
+    //   },
+    // ]),
   ],
 };
