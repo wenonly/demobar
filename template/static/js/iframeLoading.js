1ce7cf4956d0e496fb68aeeb55ca53e9c2fce2ad
Tao Wen <1325772089@qq.com>
2020-05-17 15:07:05 +0800

    update

--- template/static/js/iframeLoading.js
+++ template/static/js/iframeLoading.js
@@ -1,14 +1,15 @@
-$(document).ready(function() {
-  var iframe = document.getElementById("iframe-box");    
-  if (iframe.attachEvent) {    
-      iframe.attachEvent("onload", function() {    
-          //iframe加载完成后你需要进行的操作  
-          console.log('12222222222')
-        });    
-      } else {    
-        iframe.onload = function() {    
-          console.log('12222222222')
-                //iframe加载完成后你需要进行的操作  
-      };    
+
+
+
+  var iframe = document.getElementById("iframe-box");
+  if (iframe.attachEvent) {
+    iframe.attachEvent("onload", function () {
+      //iframe加载完成后你需要进行的操作
+      console.log("12222222222");
+    });
+  } else {
+    iframe.onload = function () {
+      console.log("12222222222");
+      //iframe加载完成后你需要进行的操作
+    };
   }
-})
