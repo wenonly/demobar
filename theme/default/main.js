08bc00a6fd803475ace9d1aa1649dbb257b5b357
陶文 <taowen@ccuol.com>
2020-11-26 17:23:30 +0800

    update  更新模版，不再需要publicpath

--- theme/default/main.js
+++ theme/default/main.js
@@ -1,44 +1,46 @@
-import './static/css/main.less'
-import './static/css/scroll.less'
-import './static/icon/iconfont.css'
-import './static/js/iframeLoading'
-import './static/css/nprogress.css'
-import { pageChange, setNavItemClickEvent, clearPath } from './static/js/page'
-import { setToggleEvent, setNavActive } from './static/js/nav'
-import { setIframeLoadingEvent } from './static/js/iframeLoading'
-import { initSearch } from './static/js/search'
-import './static/js/live2d_cat'
+import "./static/css/main.less";
+import "./static/css/scroll.less";
+import "./static/icon/iconfont.css";
+import "./static/js/iframeLoading";
+import "./static/css/nprogress.css";
+import { pageChange, setNavItemClickEvent, clearPath } from "./static/js/page";
+import { setToggleEvent, setNavActive } from "./static/js/nav";
+import { setIframeLoadingEvent } from "./static/js/iframeLoading";
+import { initSearch } from "./static/js/search";
+import "./static/js/live2d_cat";
 
 window.onhashchange = () => {
   // 设置nav活跃效果
-  initActive()
-}
+  initActive();
+};
 
 $(document).ready(function() {
   // 设置nav活跃效果
-  initActive()
+  initActive();
   // 点击a标签修改hash
-  setNavItemClickEvent()
+  setNavItemClickEvent();
   // 设置目录的展开收起
-  setToggleEvent()
+  setToggleEvent();
   // 设置iframe loading监听
-  setIframeLoadingEvent()
+  setIframeLoadingEvent();
   // 初始化搜索
   initSearch(() => {
     // 清除hash，重置路由
-    clearPath()
+    clearPath();
     // 设置nav活跃效果
-    initActive()
+    initActive();
     // 点击a标签修改hash
-    setNavItemClickEvent()
+    setNavItemClickEvent();
     // 设置目录的展开收起
-    setToggleEvent()
-  })
-})
+    setToggleEvent();
+  });
+});
 
 // * 设置nav活跃效果
 function initActive() {
-  const pathName = pageChange()
+  const pathName = pageChange();
   // 设置动态颜色
-  if(pathName) { setNavActive(pathName.slice(1)) }
+  if (pathName) {
+    setNavActive(pathName);
+  }
 }
--- theme/default/static/js/page.js
+++ theme/default/static/js/page.js
@@ -1,38 +1,37 @@
-import nProgress from 'nprogress';
+import nProgress from "nprogress";
 
 // * 监听hash变化，设置内嵌页面
 export function pageChange() {
-  const pathName = window.location.hash.substr(1)
+  const pathName = window.location.hash.substr(2);
   if (pathName && pathName.length > 1) {
-    nProgress.start()
+    nProgress.start();
     setIframeSrc(pathName);
-    return pathName
-  }
-  else {
+    return pathName;
+  } else {
     // 初始化页面后默认加载第一个页面
-    const aList = $(".list-content a")
+    const aList = $(".list-content a");
     if (aList && aList.length) {
-      const hashpath = aList[0].getAttribute("data-path")
-      window.location.hash = hashpath
+      const hashpath = aList[0].getAttribute("data-path");
+      window.location.hash = hashpath;
     }
   }
 }
 
 export function setNavItemClickEvent() {
-  $(".list-content a").click(function(e){
-    const pathName = $(this).attr("data-path")
-    window.location.hash = pathName
+  $(".list-content a").click(function(e) {
+    const pathName = $(this).attr("data-path");
+    window.location.hash = pathName;
     e.preventDefault();
   });
 }
 
 export function clearPath() {
-  window.location.hash = ''
+  window.location.hash = "";
 }
 
 // * 设置iframe标签src
 function setIframeSrc(pathName) {
-  if (pathName.length === 1) return
-  const path = pathName.slice(-1) !== '/' ? pathName + '/' : pathName
-  $('#iframe-box').attr('src', publicPath.slice(0, -1) + path);
+  if (pathName.length === 1) return;
+  const path = pathName.slice(-1) !== "/" ? pathName + "/" : pathName;
+  $("#iframe-box").attr("src", path);
 }
